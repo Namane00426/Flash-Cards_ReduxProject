014b3fd22bcdeb2393d1af24c6af07568041ce73
Nanase.E <nanase.matsui@gmail,com>
2021-11-19 15:59:51 +0100

    update to add new topics

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -3,11 +3,14 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import { addTopic } from "../features/topics/topicSlice";
+import { useDispatch } from "react-redux";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,7 +18,7 @@ export default function NewTopicForm() {
       return;
     }
 
-    // dispatch your add topic action here
+    dispatch(addTopic({name:name, id: uuidv4(), icon:icon}));
     history.push(ROUTES.topicsRoute());
   };
 
@@ -46,7 +49,9 @@ export default function NewTopicForm() {
             ))}
           </select>
         </div>
-        <button className="center">Add Topic</button>
+        <button className="center" 
+        type="submit"
+        value="Submit" >Add Topic</button>
       </form>
     </section>
   );
---
+++ src/features/topics/topicSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+// When addTopic action used, payload for it look like
+//  {id: '123456', name: 'name of topic', icon: 'icon url'}
+
+const topicsSlice = createSlice({
+  name: 'topics',
+  initialState: {
+    topics: {
+    }
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      
+      state.topics.push({ 
+        [action.payload.id]: {
+          id: id,
+          name: name,
+          icon: icon,
+          quizIds: []
+        }
+      })
+    }
+  }
+})
+
+export const {addTopic} = topicsSlice.actions;
+
+export const selectTopics = state => state.topics.topics;
+export default topicsSlice.reducer
+// console.log(topicsSlice.actions);
