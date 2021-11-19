3e9bf8149e482a03fdb189689f683b26fc4de8cc
Nanase.E <nanase.matsui@gmail,com>
2021-11-19 22:34:03 +0100

    update addQuizz func

--- src/features/topics/topicSlice.js
+++ src/features/topics/topicSlice.js
@@ -8,7 +8,14 @@ const topicsSlice = createSlice({
   name: 'topics',
   initialState: {
     topics: {
+      "cb727b37-b245-4e63-9d1d-98175aaba124": 
+      {
+      id:"cb727b37-b245-4e63-9d1d-98175aaba124",
+      name:"books",
+      icon:"https://static-assets.codecademy.com/skillpaths/react-redux/redux-quiz-app/book.svg",
+      quizIds:[]
     }
+  }
   },
   reducers: {
     addTopic: (state, action) => {
@@ -19,12 +26,15 @@ const topicsSlice = createSlice({
                 icon: icon,
                 quizIds: []
               }
-      
+    },
+    addQuizIds: (state, action) => {
+      const { topicId } = action.payload;
+      state.topics[topicId].quizIds.push(action.payload.quizId);
     }
   }
 })
 
-export const {addTopic} = topicsSlice.actions;
+export const {addTopic, addQuizIds} = topicsSlice.actions;
 
 export const selectTopics = state => state.topics.topics;
 export default topicsSlice.reducer
